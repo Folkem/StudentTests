38cafe879a170e81dd9691c14462fc281d14560f
Maks Lisitsyn <Folkem>
2021-05-17 18:17:23 +0300

    Add header
    Add header blade component with links (currently absent links).

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,9 @@
 @section('title', 'Вітаємо')
 
 @section('body')
-    <div class="w-full h-full flex">
+    @include('layouts.header')
+
+    <div class="w-full h-screen flex">
         <div class="w-auto h-auto m-auto text-4xl font-bold">
             Привіт! Ви можете перейти до різних розділів сайту за допомогою
             панелі зверху.
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+<div class="w-full p-4 h-auto fixed">
+    <div class="flex flex-row gap-4 justify-end">
+        <div>
+            <a href="#">Вчителі</a>
+        </div>
+        <div>
+            <a href="#">Вийти</a>
+        </div>
+    </div>
+</div>
