df56f12cbfd191832b5f68fe4b8de3ea655e8357
Maks Lisitsyn <Folkem>
2021-05-18 16:45:18 +0300

    Add isAdmin() method to User model
    'isAdmin()' method returns true, if user has a admin role.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,13 @@
 <div class="w-full p-4 h-auto fixed">
     <div class="flex flex-row gap-4 justify-end pr-8">
-        <div>
-            <a href="{{ route('groups.index') }}">Групи</a>
-        </div>
-        <div>
-            <a href="{{ route('teachers.index') }}">Вчителі</a>
-        </div>
+        @if(auth()->user()->isAdmin())
+            <div>
+                <a href="{{ route('groups.index') }}">Групи</a>
+            </div>
+            <div>
+                <a href="{{ route('teachers.index') }}">Вчителі</a>
+            </div>
+        @endif
         <div>
             <a href="{{ route('logout') }}">Вийти</a>
         </div>
